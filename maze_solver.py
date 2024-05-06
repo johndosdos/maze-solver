bf4db5c2bc91b174a82e129b3898b2eed3df563e
John <128350391+alpaOo@users.noreply.github.com>
2024-05-06 10:24:50 +0800

    Initialize Window class for maze solver app
    This program implements python's tkinter library for GUI. The class
    utilizes tkinter for window and title creation and handling program
    termination, while containing several methods for managing user
    interactions

---
+++ maze_solver.py
@@ -0,0 +1,33 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.root = Tk()
+        self.root.title("Simple maze solver")
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+        self.canvas = Canvas()
+        self.canvas.pack()
+
+        self.is_running = False
+
+    def redraw(self):
+        self.root.update()
+        self.root.update_idletasks()
+
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running:
+            self.redraw()
+
+    def close(self):
+        self.is_running = False
+
+
+def main():
+    win = Window(800, 800)
+    win.wait_for_close()
+
+
+main()
