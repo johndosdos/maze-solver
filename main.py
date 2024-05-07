da3dba8fdd8643881b43c587146c5c5febc5c367
John <128350391+alpaOo@users.noreply.github.com>
2024-05-08 03:05:07 +0800

    Update main function

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ from cell import *
 
 def main():
     win = Window(800, 600)
-    cell1 = Cell(Point(10, 10), Point(100, 100))
-    cell2 = Cell(Point(100, 10), Point(190, 100))
-    win.draw_cell(cell1, "white")
-    win.draw_cell(cell2, "white")
+
+    cell_1 = Cell(Point(10, 10), Point(100, 100), win)
+    cell_2 = Cell(Point(100, 10), Point(190, 100), win)
+    cell_3 = Cell(Point(190, 10), Point(280, 100), win)
+    cell_4 = Cell(Point(190, 100), Point(280, 190), win)
+
+    win.draw_cell(cell_1, "white")
+    win.draw_cell(cell_2, "white")
+    win.draw_cell(cell_3, "white")
+    win.draw_cell(cell_4, "white")
+
+    cell_1.draw_move(cell_3)
+    cell_3.draw_move(cell_4)
+
     win.wait_for_close()
 
 
