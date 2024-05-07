ae4e9181c660d90829169fed43cf3a548a469b0e
John <128350391+alpaOo@users.noreply.github.com>
2024-05-08 00:40:04 +0800

    Move cell creation function to separate file
    Separated Cell class to cell.py for organization

---
+++ cell.py
@@ -0,0 +1,26 @@
+class Cell:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        self.has_top_wall = True
+        self.has_right_wall = True
+        self.has_bottom_wall = True
+        self.has_left_wall = True
+
+    def draw(self, canvas, fill_color):
+        if self.has_top_wall:
+            canvas.create_line(
+                self.p1.x, self.p1.y, self.p2.x, self.p1.y, fill=fill_color, width=2
+            )
+        if self.has_right_wall:
+            canvas.create_line(
+                self.p2.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+            )
+        if self.has_bottom_wall:
+            canvas.create_line(
+                self.p2.x, self.p2.y, self.p1.x, self.p2.y, fill=fill_color, width=2
+            )
+        if self.has_left_wall:
+            canvas.create_line(
+                self.p1.x, self.p2.y, self.p1.x, self.p1.y, fill=fill_color, width=2
+            )
