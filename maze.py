4df926eb83c2f8973fc64ba91ab76cc45f6649e9
John <128350391+alpaOo@users.noreply.github.com>
2024-05-13 21:14:45 +0800

    Rename maze.py to maze.py
    Removed whitespace from filename

---
+++ maze.py
@@ -0,0 +1,20 @@
+from cell import *
+from graphics import Point
+
+
+class Maze:
+    def __init__(self, num_rows, num_cols, cell_size_x, cell_size_y):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.cells = []
+
+    def create_cells(self):
+        pass
+
+    def draw_cell(self, i, j):
+        pass
+
+    def animate(self):
+        pass
