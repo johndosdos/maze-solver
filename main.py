35b55bf0cd40c0dcf5e083d0823bcfefbfecb8a9
John <128350391+alpaOo@users.noreply.github.com>
2024-05-14 21:41:16 +0800

    Add tests for the maze Python module in main.py
    This update tests the functionality of the Maze
    class to verify if the cells are properly rendered
    on the canvas

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from graphics import *
 from cell import *
+from maze import *
 
 
 def main():
     win = Window(800, 600)
 
-    cell_1 = Cell(Point(10, 10), Point(100, 100), win)
-    cell_2 = Cell(Point(100, 10), Point(190, 100), win)
-    cell_3 = Cell(Point(190, 10), Point(280, 100), win)
-    cell_4 = Cell(Point(190, 100), Point(280, 190), win)
-
-    win.draw_cell(cell_1, "white")
-    win.draw_cell(cell_2, "white")
-    win.draw_cell(cell_3, "white")
-    win.draw_cell(cell_4, "white")
-
-    cell_1.draw_move(cell_3)
-    cell_3.draw_move(cell_4)
+    x = 10
+    y = 10
+    num_rows = 5
+    num_cols = 5
+    cell_size_x = 50  # pixel
+    cell_size_y = 50  # pixel
+    maze = Maze(x, y, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    maze.create_cells()
 
     win.wait_for_close()
 
